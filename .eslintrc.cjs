23c806f374273320d3e210f0460072b0833e5661
IamfrancisIwu <Francis.Iwu@teclead-ventures.de>
2025-09-24 12:08:25 +0200

    fix: disable Turbopack for production build to avoid PostCSS crash

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   root: true,
-  extends: ['next'],
+  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
