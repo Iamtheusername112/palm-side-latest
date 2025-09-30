19a4ec486b229d3414cb3a4ff5f5e960ac1d2c79
Ilona Vichnevskaia <ilonavich@yahoo.de>
2025-09-30 19:27:19 +0200

    update current year

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -25,7 +25,9 @@ const TikTokIcon = ({ className = '' }) => (
 )
 
 const Footer = () => {
+  const startYear = 2025
   const currentYear = new Date().getFullYear()
+  const yearText = startYear === currentYear ? `${currentYear}` : `${startYear}–${currentYear}`
   const [openAccordions, setOpenAccordions] = useState({})
 
   const toggleAccordion = (section) => {
@@ -328,10 +330,7 @@ const Footer = () => {
         <div className='border-t border-gray-500 mt-8 pt-8'>
           <div className='flex flex-col md:flex-row justify-between items-center'>
             <p className='text-gray-400 text-sm'>
-              © {currentYear}{' '}
-              <Translate staticKey='footer.copyright'>
-                Palmside S.L. All rights reserved.
-              </Translate>
+              © {yearText} Palmside S.L. All rights reserved.
             </p>
             <div className='flex space-x-6 mt-4 md:mt-0'>
               <Link
